fac3f06e449d14a908ebf3f85afa48ac817a547a
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 15:55:49 -0600

    Update EconomyController.cs

--- Scripts/Economy/EconomyController.cs
+++ Scripts/Economy/EconomyController.cs
@@ -15,10 +15,25 @@ namespace AsTheBellTolls.Economy {
 public class EconomyController : Singleton<EconomyController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Sub Controllers")]
+    [SerializeField] private CommissionsSubController _commissionsSubController;
+    [SerializeField] private EconomySubController _economySubController;
+    [SerializeField] private FestivalsSubController _festivalsSubController;
+    [SerializeField] private MailSubController _mailSubController;
+    [SerializeField] private QuestsSubController _questsSubController;
+    [SerializeField] private ShopsSubController _shopsSubController;
+    [SerializeField] private TownRankingSubController _townRankingSubController;
+    [SerializeField] private TownWealthSubController _townWealthSubController;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public CommissionsSubController Commissions => _commissionsSubController;
+    public EconomySubController Economy => _economySubController;
+    public FestivalsSubController Festivals => _festivalsSubController;
+    public MailSubController Mail => _mailSubController;
+    public QuestsSubController Quests => _questsSubController;
+    public ShopsSubController Shops => _shopsSubController;
+    public TownRankingSubController TownRanking => _townRankingSubController;
+    public TownWealthSubController TownWealth => _townWealthSubController;
 #endregion
 #region -------------------- Private Variables --------------------
     
