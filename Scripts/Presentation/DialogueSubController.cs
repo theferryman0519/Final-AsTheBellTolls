8455d9e8b73ffcff69c17c8d5bf54ea1927137fc
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 15:47:29 -0600

    Create DialogueSubController.cs

---
+++ Scripts/Presentation/DialogueSubController.cs
@@ -0,0 +1,39 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Presentation.Dialogue;
+using AsTheBellTolls.Infrastructure.Core;
+
+namespace AsTheBellTolls.Presentation {
+public class DialogueSubController : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
