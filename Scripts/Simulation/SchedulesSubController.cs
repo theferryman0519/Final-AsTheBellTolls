51db173f2dfa4a7c2fcfd5a9383cc36e9db734ba
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 15:50:44 -0600

    Create SchedulesSubController.cs

---
+++ Scripts/Simulation/SchedulesSubController.cs
@@ -0,0 +1,39 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Simulation.Schedules;
+using AsTheBellTolls.Infrastructure.Core;
+
+namespace AsTheBellTolls.Simulation {
+public class SchedulesSubController : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
