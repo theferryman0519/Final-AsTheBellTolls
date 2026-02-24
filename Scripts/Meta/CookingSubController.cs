f77981f74fdd8f09b4e08b3eeddf4404f4e1872d
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 15:39:20 -0600

    Create CookingSubController.cs

---
+++ Scripts/Meta/CookingSubController.cs
@@ -0,0 +1,39 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Meta.Cooking;
+using AsTheBellTolls.Infrastructure.Core;
+
+namespace AsTheBellTolls.Meta {
+public class CookingSubController : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
