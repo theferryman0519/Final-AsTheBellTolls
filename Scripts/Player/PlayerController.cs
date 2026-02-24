7ff402ae1563a5cb3f249e0ef435d5e7b2d1641a
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 16:07:24 -0600

    Update PlayerController.cs

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -15,10 +15,21 @@ namespace AsTheBellTolls.Player {
 public class PlayerController : Singleton<PlayerController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Sub Controllers")]
+    [SerializeField] private CameraSubController _cameraSubController;
+    [SerializeField] private FamilySubController _familySubController;
+    [SerializeField] private InputSubController _inputSubController;
+    [SerializeField] private PlayerSubController _playerSubController;
+    [SerializeField] private SkillTreeSubController _skillTreeSubController;
+    [SerializeField] private TimeManipulationSubController _timeManipulationSubController;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public CameraSubController Camera => _cameraSubController;
+    public FamilySubController Family => _familySubController;
+    public InputSubController Input => _inputSubController;
+    public PlayerSubController Player => _playerSubController;
+    public SkillTreeSubController SkillTree => _skillTreeSubController;
+    public TimeManipulationSubController TimeManipulation => _timeManipulationSubController;
 #endregion
 #region -------------------- Private Variables --------------------
     
