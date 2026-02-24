be9326832c507c50a1813b090dbb5df1fe5eb721
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 15:29:30 -0600

    Create GameTimeSubController.cs

---
+++ Scripts/Infrastructure/GameTimeSubController.cs
@@ -0,0 +1,39 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Infrastructure.GameTime;
+using AsTheBellTolls.Infrastructure.Core;
+
+namespace AsTheBellTolls.Infrastructure {
+public class GameTimeSubController : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
