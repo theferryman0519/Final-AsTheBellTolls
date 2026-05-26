1de077369b86b71307cd8d2db6166650e25a8262
The Ferryman <ferrymanstudios@gmail.com>
2026-05-26 11:03:18 -0500

    Update PlayerController.cs

--- Scripts/Controller/PlayerController.cs
+++ Scripts/Controller/PlayerController.cs
@@ -15,7 +15,11 @@ namespace Atbt.Controller {
 public class PlayerController : Singleton<PlayerController> {
 
 #region -------------------- Serialized Variables --------------------
+    [Header("Player Movement")]
+    [SerializeField] private PlayerMovement Movement;
 
+    [Header("Player Stamina")]
+    [SerializeField] private PlayerStamina Stamina;
 #endregion
 #region -------------------- Public Variables --------------------
 
@@ -35,6 +39,16 @@ public class PlayerController : Singleton<PlayerController> {
     // Controls player interactions
     // Controls player tool usage
     // Controls state of the player
+
+    public void Initialize(Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the player controller");
+
+        if (Movement == null) { gameobject.AddComponent<PlayerMovement>(); }
+        if (Stamina == null) { gameobject.AddComponent<PlayerStamina>(); }
+
+        CoreController.Inst.LoadingStepCompleted();
+    }
 #endregion
 #region -------------------- Private Methods --------------------
 
