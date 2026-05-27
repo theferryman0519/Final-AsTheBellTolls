ce751c954e238dd33303294d425bfa20b915eb4e
The Ferryman <ferrymanstudios@gmail.com>
2026-05-27 08:18:43 -0500

    Create ConstantController.cs

---
+++ Scripts/Controller/ConstantController.cs
@@ -0,0 +1,38 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using Atbt.Core;
+
+namespace Atbt.Controller {
+public class ConstantController : Singleton<ConstantController> {
+
+#region -------------------- Serialized Variables --------------------
+
+#endregion
+#region -------------------- Public Variables --------------------
+
+#endregion
+#region -------------------- Private Variables --------------------
+
+#endregion
+#region -------------------- Initial Functions --------------------
+
+#endregion
+#region -------------------- Coroutines --------------------
+
+#endregion
+#region -------------------- Public Methods --------------------
+    // Controls all constant variables
+#endregion
+#region -------------------- Private Methods --------------------
+
+#endregion
+}}
