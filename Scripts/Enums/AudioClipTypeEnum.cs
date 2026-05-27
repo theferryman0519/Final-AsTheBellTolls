172554b90ff66f873ee4089705bb6a70f0273899
The Ferryman <ferrymanstudios@gmail.com>
2026-05-27 15:24:48 -0500

    Create AudioClipTypeEnum.cs

---
+++ Scripts/Enums/AudioClipTypeEnum.cs
@@ -0,0 +1,21 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum AudioClipTypeEnum {
+    None = 0,
+    Music = 1,
+    Speech = 2,
+    Ambiance = 3,
+    Effects = 4,
+    Footsteps = 5
+}}
