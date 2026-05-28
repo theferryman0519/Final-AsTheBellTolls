cf59eae5509779cc479d2baca830159118915b41
The Ferryman <ferrymanstudios@gmail.com>
2026-05-28 16:15:30 -0500

    Create PauseMenuTypeEnum.cs

---
+++ Scripts/Enums/Ui/PauseMenuTypeEnum.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum PauseMenuTypeEnum {
+    Pause = 0,
+    Settings = 1,
+    Controls = 2,
+    Report = 3
+}}
