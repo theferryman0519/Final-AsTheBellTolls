f4529af9abfc2ed3a327a0f2c3b165d710822075
The Ferryman <ferrymanstudios@gmail.com>
2026-05-28 15:58:09 -0500

    Create UiElementDialogue.cs

---
+++ Scripts/Methods/Ui/UiElementDialogue.cs
@@ -0,0 +1,38 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Ui {
+public class UiElementDialogue : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Canvas Element")]
+    [SerializeField] private Canvas CanvasElement;
+#endregion
+#region -------------------- Public Variables --------------------
+    public Canvas MainCanvas => CanvasElement;
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
