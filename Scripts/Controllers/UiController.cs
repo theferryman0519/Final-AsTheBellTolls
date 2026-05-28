f8da2d4953839e3f42ad69f6c223cde686a6ca80
The Ferryman <ferrymanstudios@gmail.com>
2026-05-28 14:50:41 -0500

    Update UiController.cs

--- Scripts/Controllers/UiController.cs
+++ Scripts/Controllers/UiController.cs
@@ -1,4 +1,5 @@
 // Main Dependencies
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,7 +16,15 @@ namespace Atbt.Controller {
 public class UiController : Singleton<UiController> {
 
 #region -------------------- Serialized Variables --------------------
-
+    [Header("UI Elements")]
+    [SerializeField] private UiElementHud Hud;
+    [SerializeField] private UiElementPause Pause;
+    [SerializeField] private UiElementDialogue Dialogue;
+    [SerializeField] private UiElementInventory Inventory;
+    [SerializeField] private UiElementItemTransfer ItemTransfer;
+    [SerializeField] private UiElementQuests Quests;
+    [SerializeField] private UiElementInventions Inventions;
+    [SerializeField] private UiElementNightSummary NightSummary;
 #endregion
 #region -------------------- Public Variables --------------------
 
@@ -41,8 +50,82 @@ public class UiController : Singleton<UiController> {
 
         CoreController.Inst.LoadingStepCompleted();
     }
+
+    public void ShowHideHud(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the HUD");
+
+        FadeCanvas(Hud.MainCanvas, isShowing);
+    }
+
+    public void ShowHidePauseMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the pause menu");
+
+        FadeCanvas(Pause.MainCanvas, isShowing);
+    }
+
+    public void ShowHideDialogueMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the dialogue menu");
+
+        FadeCanvas(Dialogue.MainCanvas, isShowing);
+    }
+
+    public void ShowHideInventoryMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the inventory menu");
+
+        FadeCanvas(Inventory.MainCanvas, isShowing);
+    }
+
+    public void ShowHideItemTransferMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the item transfer menu");
+
+        FadeCanvas(ItemTransfer.MainCanvas, isShowing);
+    }
+
+    public void ShowHideQuestsMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the quests menu");
+
+        FadeCanvas(Quests.MainCanvas, isShowing);
+    }
+
+    public void ShowHideInventionsMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the inventions tree menu");
+
+        FadeCanvas(Inventions.MainCanvas, isShowing);
+    }
+
+    public void ShowHideNightSummaryMenu(bool isShowing)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Showing/hiding the end-of-day summary menu");
+
+        FadeCanvas(NightSummary.MainCanvas, isShowing);
+    }
 #endregion
 #region -------------------- Private Methods --------------------
+    private void FadeCanvas(Canvas canvas, bool isFadingIn, Action continueAction = null)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Fading canvas element");
 
+        Sequence sequence = DOTween.Sequence();
+
+        float initialAlpha = isFadingIn ? 0f : 1f;
+        float finalAlpha = isFadingIn ? 1f : 0f;
+
+        canvas.alpha = initialAlpha;
+        sequence.Join(canvas.DOFade(finalAlpha, ConstantController.Multiplier_Fading));
+
+        sequence.OnComplete(() =>
+        {
+            continueAction?.Invoke();
+        });
+
+        sequence.Play();
+    }
 #endregion
 }}
