3231f60982e9c0c773df4b31b68bffdce747eeb0
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 09:49:46 -0500

    Create AtbtButton.cs

---
+++ Scripts/Methods/Ui/Base/AtbtButton.cs
@@ -0,0 +1,54 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using Atbt.Controller;
+
+namespace Atbt.Ui {
+public class AtbtButton : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    [Header("Button Element")]
+    [SerializeField] private Button ButtonElement;
+    
+    [Header("Text Element")]
+    [SerializeField] private TMP_Text TextElement;
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    public void SetInteractability(bool isActive)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the button interactability");
+
+        ButtonElement.interactable = isActive;
+    }
+    
+    public void SetText(string content)
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the text content");
+
+        TextElement.text = content;
+    }
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
