71bbb6a15ecad53099ed24cc107831d8d5fbb76a
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 11:18:32 -0500

    Create WeekdayTypeEnum.cs

---
+++ Scripts/Enums/Time/WeekdayTypeEnum.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum WeekdayTypeEnum {
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6
+}}
