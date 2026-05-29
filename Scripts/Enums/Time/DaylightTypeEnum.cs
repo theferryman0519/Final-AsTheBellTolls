05d48a5e79eb6dc11bd6322f73f01f115f7c848f
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 11:16:19 -0500

    Create DaylightTypeEnum.cs

---
+++ Scripts/Enums/Time/DaylightTypeEnum.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum DaylightTypeEnum {
+    Dawn = 0,
+    Day = 1,
+    Dusk = 2,
+    Night = 3
+}}
