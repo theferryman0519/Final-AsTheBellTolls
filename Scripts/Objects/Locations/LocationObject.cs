640027b996f561b329728605e7f5c9bcf412d7c5
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 09:25:53 -0500

    Create LocationObject.cs

---
+++ Scripts/Objects/Locations/LocationObject.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Object {
+[CreateAssetMenu(menuName = "Objects/Location")]
+public class LocationObject : ScriptableObject {
+    public string Id;
+    public string DisplayName;
+    public string CorrespondingScene;
+    public string CorrespondingAudio;
+}}
