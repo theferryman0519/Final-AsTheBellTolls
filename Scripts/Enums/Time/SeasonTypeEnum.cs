6fa1b09733c135c0aed32f25f7c10bfb09d0db3d
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 11:13:37 -0500

    Create SeasonTypeEnum.cs

---
+++ Scripts/Enums/Time/SeasonTypeEnum.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum SeasonTypeEnum {
+    Spring = 0,
+    Summer = 1,
+    Autumn = 2,
+    Winter = 3
+}}
