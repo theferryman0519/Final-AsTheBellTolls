12cd856bd0bbce9baec3538ed856cb9e4da5fe49
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 09:46:05 -0500

    Update UiElementHud.cs

--- Scripts/Methods/Ui/Elements/UiElementHud.cs
+++ Scripts/Methods/Ui/Elements/UiElementHud.cs
@@ -9,6 +9,8 @@ using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 // Game Dependencies
+using Atbt.Controller;
+using Atbt.Object;
 
 namespace Atbt.Ui {
 public class UiElementHud : MonoBehaviour {
@@ -16,6 +18,12 @@ public class UiElementHud : MonoBehaviour {
 #region -------------------- Serialized Variables --------------------
     [Header("Canvas Element")]
     [SerializeField] private Canvas CanvasElement;
+
+    [Header("Text Objects")]
+    [SerializeField] private AtbtText DateText;
+    [SerializeField] private AtbtText TimeText;
+    [SerializeField] private AtbtText LocationText;
+    [SerializeField] private AtbtText InteractText;
 #endregion
 #region -------------------- Public Variables --------------------
     public Canvas MainCanvas => CanvasElement;
@@ -30,7 +38,20 @@ public class UiElementHud : MonoBehaviour {
     
 #endregion
 #region -------------------- Public Methods --------------------
-    
+    public void UpdateLocation()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the location text");
+
+        LocationObject location = LocationController.Inst.GetCurrentLocation();
+        string locationName = "Unknown Location";
+
+        if (location != null)
+        {
+            locationName = locationName.DisplayName;
+        }
+
+        LocationText.SetText(locationName);
+    }
 #endregion
 #region -------------------- Private Methods --------------------
     
