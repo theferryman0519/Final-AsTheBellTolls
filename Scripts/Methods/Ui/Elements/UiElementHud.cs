f2e65996f483de133cc5e8fcd780ade17a056a41
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 13:07:21 -0500

    Update UiElementHud.cs

--- Scripts/Methods/Ui/Elements/UiElementHud.cs
+++ Scripts/Methods/Ui/Elements/UiElementHud.cs
@@ -10,6 +10,7 @@ using UnityEngine.SceneManagement;
 
 // Game Dependencies
 using Atbt.Controller;
+using Atbt.Enum;
 using Atbt.Object;
 
 namespace Atbt.Ui {
@@ -24,6 +25,10 @@ public class UiElementHud : MonoBehaviour {
     [SerializeField] private AtbtText TimeText;
     [SerializeField] private AtbtText LocationText;
     [SerializeField] private AtbtText InteractText;
+
+    [Header("Icon Elements")]
+    [SerializeField] private Image DaylightIcon;
+    [SerializeField] private Image WeatherIcon;
 #endregion
 #region -------------------- Public Variables --------------------
     public Canvas MainCanvas => CanvasElement;
@@ -38,6 +43,20 @@ public class UiElementHud : MonoBehaviour {
     
 #endregion
 #region -------------------- Public Methods --------------------
+    public void UpdateDate()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the date text");
+
+        DateText.SetText(TimeController.Inst.GetCurrentDate());
+    }
+
+    public void UpdateTime()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the time text");
+
+        TimeText.SetText(TimeController.Inst.GetCurrentTime());
+    }
+
     public void UpdateLocation()
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the location text");
@@ -51,6 +70,24 @@ public class UiElementHud : MonoBehaviour {
         }
 
         LocationText.SetText(locationName);
+    }
+
+    public void UpdateDaylightIcon()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the daylight icon");
+
+        DaylightTypeEnum currentDaylight = TimeController.Inst.GetCurrentDaylight();
+
+        // TODO
+        // Set DaylightIcon sprite from SpriteController based on current time
+    }
+
+    public void UpdateWeatherIcon()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the weather icon");
+
+        // TODO
+        // Set WeatherIcon sprite from SpriteController based on weather
     }
 #endregion
 #region -------------------- Private Methods --------------------
