18e37f28075e7ecc2fd35acc6ca1ac6ec44ee2c0
The Ferryman <ferrymanstudios@gmail.com>
2026-05-29 09:24:43 -0500

    Create OBJECT_TEMPLATE.cs

---
+++ Scripts/Templates/OBJECT_TEMPLATE.cs
@@ -0,0 +1,17 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Object {
+[CreateAssetMenu(menuName = "Objects/TEMPLATE")]
+public class OBJECT_TEMPLATE : ScriptableObject {
+    
+}}
