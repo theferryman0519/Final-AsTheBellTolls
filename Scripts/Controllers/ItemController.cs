79716c7f1686566375bff9e054743d53234641d6
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 13:39:00 -0500

    Update ItemController.cs

--- Scripts/Controllers/ItemController.cs
+++ Scripts/Controllers/ItemController.cs
@@ -15,7 +15,29 @@ namespace Atbt.Controller {
 public class ItemController : Singleton<ItemController> {
 
 #region -------------------- Serialized Variables --------------------
-
+    [Header("Item Objects")]
+    [SerializeField] private List<ItemObject> ArtifactsList = new();
+    [SerializeField] private List<ItemObject> BooksList = new();
+    [SerializeField] private List<ItemObject> ByproductsList = new();
+    [SerializeField] private List<ItemObject> ClothingList = new();
+    [SerializeField] private List<ItemObject> CropsList = new();
+    [SerializeField] private List<ItemObject> DecorList = new();
+    [SerializeField] private List<ItemObject> DiscsList = new();
+    [SerializeField] private List<ItemObject> DrinksList = new();
+    [SerializeField] private List<ItemObject> FabricationsList = new();
+    [SerializeField] private List<ItemObject> FishList = new();
+    [SerializeField] private List<ItemObject> FlowersList = new();
+    [SerializeField] private List<ItemObject> FurnitureList = new();
+    [SerializeField] private List<ItemObject> GemstonesList = new();
+    [SerializeField] private List<ItemObject> HerbsList = new();
+    [SerializeField] private List<ItemObject> InventionsList = new();
+    [SerializeField] private List<ItemObject> MealsList = new();
+    [SerializeField] private List<ItemObject> OresList = new();
+    [SerializeField] private List<ItemObject> SeedsList = new();
+    [SerializeField] private List<ItemObject> StonesList = new();
+    [SerializeField] private List<ItemObject> TonicsList = new();
+    [SerializeField] private List<ItemObject> ToolsList = new();
+    [SerializeField] private List<ItemObject> WoodsList = new();
 #endregion
 #region -------------------- Public Variables --------------------
 
