99cb2c5a837ee6564570da174b689bbb939c506b
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 15:16:04 -0500

    Create QualityTypeEnum.cs

---
+++ Scripts/Enums/Items/QualityTypeEnum.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum QualityTypeEnum {
+    Base = 0,
+    Copper = 1,
+    Silver = 2,
+    Gold = 3,
+    Cobalt = 4,
+}}
