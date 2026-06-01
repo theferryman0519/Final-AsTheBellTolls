2392cabb434382cbbf351908f564afe1c4355c25
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 16:01:38 -0500

    Create InventorySortEnum.cs

---
+++ Scripts/Enums/Inventory/InventorySortEnum.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum InventorySortEnum {
+    AZ = 0,
+    ZA = 1,
+    HighLow = 2,
+    LowHigh = 3,
+    BaseCobalt = 4,
+    CobaltBase = 5,
+    Type = 6,
+}}
