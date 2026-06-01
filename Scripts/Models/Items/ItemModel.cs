c865ba913f4cdf46cf6329acf091674017b974a2
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 15:15:03 -0500

    Create ItemModel.cs

---
+++ Scripts/Models/Items/ItemModel.cs
@@ -0,0 +1,26 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using Atbt.Enum;
+using Atbt.Object;
+
+namespace Atbt.Item {
+public class ItemModel {
+    
+#region -------------------- Variables --------------------
+    public ItemObject Item;
+
+    public int Count;
+
+    public QualityTypeEnum Quality;
+    public RipenessTypeEnum Ripeness;
+#endregion
+}}
