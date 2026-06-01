53196f9e95ea98e02ade6fdf7bcadb56a829e1ee
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 13:19:40 -0500

    Create ItemTypeEnum.cs

---
+++ Scripts/Enums/Item/ItemTypeEnum.cs
@@ -0,0 +1,37 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum ItemTypeEnum {
+    Artifact = 0,
+    Book = 1,
+    Byproduct = 2,
+    Clothing = 3,
+    Crop = 4,
+    Decor = 5,
+    Disc = 6,
+    Drink = 7,
+    Fabrication = 8,
+    Fish = 9,
+    Flower = 10,
+    Furniture = 11,
+    Gemstone = 12,
+    Herb = 13,
+    Invention = 14,
+    Meal = 15,
+    Ore = 16,
+    Seed = 17,
+    Stone = 18,
+    Tonic = 19,
+    Tool = 20,
+    Wood = 21,
+}}
