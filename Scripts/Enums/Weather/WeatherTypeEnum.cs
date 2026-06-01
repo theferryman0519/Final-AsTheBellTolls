98b7172ab63d8d4af6b5dde28aad204f4c0bab4f
The Ferryman <ferrymanstudios@gmail.com>
2026-06-01 13:40:29 -0500

    Create WeatherTypeEnum.cs

---
+++ Scripts/Enums/Weather/WeatherTypeEnum.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum WeatherTypeEnum {
+    Clear = 0,
+    Cloudy = 1,
+    Rainy = 2,
+    SevereRainy = 3,
+    Snowy = 4,
+    SevereSnowy = 5,
+    Windy = 6,
+}}
