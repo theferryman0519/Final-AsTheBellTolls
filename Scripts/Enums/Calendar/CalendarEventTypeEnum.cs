2d0fc394232cb68dd6d454478eb6eb0324de3008
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 10:19:43 -0500

    Create CalendarEventTypeEnum.cs

---
+++ Scripts/Enums/Calendar/CalendarEventTypeEnum.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum CalendarEventTypeEnum {
+    Birthday = 0,
+    Anniversary = 1,
+    MainFestival = 2,
+    MiniFestival = 3,
+    OngoingEvent = 4,
+}}
