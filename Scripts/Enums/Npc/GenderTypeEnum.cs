80511b6680114ebb90345b16a1b76676fe230eb0
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:31:53 -0500

    Create GenderTypeEnum.cs

---
+++ Scripts/Enums/Npc/GenderTypeEnum.cs
@@ -0,0 +1,18 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum GenderTypeEnum {
+    Female = 0,
+    Male = 1,
+    NonBinary = 2,
+}}
