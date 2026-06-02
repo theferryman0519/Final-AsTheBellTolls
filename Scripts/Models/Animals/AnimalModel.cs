c8e1abeb7967226e759d3e3f140056be3d78bee4
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 14:33:15 -0500

    Create AnimalModel.cs

---
+++ Scripts/Models/Animals/AnimalModel.cs
@@ -0,0 +1,18 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Animal {
+public class AnimalModel {
+#region -------------------- Variables --------------------
+    
+#endregion
+}}
