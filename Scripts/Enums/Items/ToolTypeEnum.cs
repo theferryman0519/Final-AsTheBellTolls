f22558f826bf012913e694e68886d60162b8878a
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 11:13:10 -0500

    Create ToolTypeEnum.cs

---
+++ Scripts/Enums/Items/ToolTypeEnum.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum ToolTypeEnum {
+    None = 0,
+    Axe = 1,
+    FishingNet = 2,
+    FishingRod = 3,
+    Hammer = 4,
+    HerbKnife = 5,
+    Hoe = 6,
+    MilkPail = 7,
+    Pickaxe = 8,
+    Scythe = 9,
+    Shears = 10,
+    WateringCan = 11,
+}}
