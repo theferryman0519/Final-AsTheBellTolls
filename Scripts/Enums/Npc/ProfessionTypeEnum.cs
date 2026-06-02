2172ad862430201bed2334c29b6e60a89230ef8a
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:38:36 -0500

    Create ProfessionTypeEnum.cs

---
+++ Scripts/Enums/Npc/ProfessionTypeEnum.cs
@@ -0,0 +1,43 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum ProfessionTypeEnum {
+    Apothecary = 0,
+    Architect = 1,
+    Baker = 2,
+    Banker = 3,
+    Bellwarden = 4,
+    Blacksmith = 5,
+    Butler = 6,
+    Carpenter = 7,
+    Chef = 8,
+    Child = 9,
+    Curator = 10,
+    Deceased = 11,
+    Elder = 12,
+    Farmer = 13,
+    Fisher = 14,
+    Gravekeeper = 15,
+    Innkeeper = 16,
+    Inventor = 17,
+    Librarian = 18,
+    Mayor = 19,
+    Miner = 20,
+    Postmaster = 21,
+    Rancher = 22,
+    Sheriff = 23,
+    StoreOwner = 24,
+    Tailor = 25,
+    Teacher = 26,
+    Trader = 27,
+}}
