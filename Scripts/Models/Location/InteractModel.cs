f733275a5c9e136c361b6a32e6580f4d17bbe12c
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 11:54:23 -0500

    Create InteractModel.cs

---
+++ Scripts/Models/Location/InteractModel.cs
@@ -0,0 +1,23 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using Atbt.Enum;
+
+namespace Atbt.Location {
+public class InteractModel {
+    
+#region -------------------- Variables --------------------
+    public ToolTypeEnum RequiredTool;
+    public QualityTypeEnum LowestQualityRequired;
+
+    public Action InteractAction;
+#endregion
+}}
