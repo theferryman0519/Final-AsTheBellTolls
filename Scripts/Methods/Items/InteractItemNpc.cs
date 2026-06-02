f04a25564455c8f1626f6a2c1ceffef7105113fd
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 14:31:43 -0500

    Add InteractItemNpc class for NPC interactions

---
+++ Scripts/Methods/Items/InteractItemNpc.cs
@@ -0,0 +1,34 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using Atbt.Enum;
+
+namespace Atbt.Item {
+public class InteractItemNpc : InteractModel {
+#region -------------------- Private Variables --------------------
+    [SerializeField] private NpcModel Npc;
+    
+    private QualityTypeEnum _lowestQualityRequired = QualityTypeEnum.Base;
+
+    private ToolTypeEnum[] _requiredTools = { ToolTypeEnum.None};
+#endregion
+#region -------------------- Public Variables --------------------
+    public QualityTypeEnum LowestQualityRequired => _lowestQualityRequired;
+    
+    public ToolTypeEnum[] RequiredTools => _requiredTools;
+#endregion
+#region -------------------- Methods --------------------
+    public override void Interact()
+    {
+        // TODO
+    }
+#endregion
+}}
