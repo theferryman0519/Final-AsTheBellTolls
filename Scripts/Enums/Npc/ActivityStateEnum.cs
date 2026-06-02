6109cdb1fc0f047be8412a720f4181eda3846e2e
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:56:37 -0500

    Create ActivityStateEnum.cs

---
+++ Scripts/Enums/Npc/ActivityStateEnum.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum ActivityStateEnum {
+    Idle = 0,
+    Walking = 1,
+    Running = 2,
+    Talking = 3,
+    GivingGift = 4,
+    ReceivingGift = 5,
+    Eating = 6,
+    Drinking = 7,
+    Sitting = 8,
+    Loving = 9,
+    UsingItem = 10,
+    UsingTool = 11,
+    UsingInvention = 12,
+    Sleeping = 13,
+    Cinematic = 14,
+}}
