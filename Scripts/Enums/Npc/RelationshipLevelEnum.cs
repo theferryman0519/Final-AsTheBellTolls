85fc33936207c57a6538f37a9845075d5a03ea8d
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:43:51 -0500

    Create RelationshipLevelEnum.cs

---
+++ Scripts/Enums/Npc/RelationshipLevelEnum.cs
@@ -0,0 +1,25 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum RelationshipLevelEnum {
+    Stranger = 0,
+    Acquaintance = 1,
+    Friendly = 2,
+    Friend = 3,
+    CloseFriend = 4,
+    Trusted = 5,
+    Betrothed = 6,
+    Spouse = 7,
+    SoulMate = 8,
+    Child = 9,
+}}
