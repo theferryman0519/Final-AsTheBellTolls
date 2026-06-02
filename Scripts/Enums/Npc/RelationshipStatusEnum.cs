30e06eb13ff0f7f86561f135643d9ccaa7418391
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:27:30 -0500

    Create RelationshipStatusEnum.cs

---
+++ Scripts/Enums/Npc/RelationshipStatusEnum.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum RelationshipStatusEnum {
+    Single = 0,
+    Dating = 1,
+    Engaged = 2,
+    Married = 3,
+    Widowed = 4,
+}}
