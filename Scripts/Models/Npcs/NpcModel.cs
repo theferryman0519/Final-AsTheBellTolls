0a031669520ec226099ebe248a06e8b5dfcb2bba
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:44:43 -0500

    Update NpcModel.cs

--- Scripts/Models/Npcs/NpcModel.cs
+++ Scripts/Models/Npcs/NpcModel.cs
@@ -9,6 +9,8 @@ using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 // Game Dependencies
+using Atbt.Enum;
+using Atbt.Object;
 
 namespace Atbt.Npc {
 public class NpcModel {
@@ -17,5 +19,12 @@ public class NpcModel {
     public NpcObject Npc;
 
     public int RelationshipPoints;
+    public int HeartCount;
+
+    public RelationshipStatusEnum RelationshipStatus;
+    public RelationshipLevelEnum RelationshipLevel;
+
+    public LocationObject Residence;
+    public LocationObject Workplace;
 #endregion
 }}
