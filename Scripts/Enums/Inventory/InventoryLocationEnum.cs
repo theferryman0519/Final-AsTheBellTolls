6896be0b82a63588812caaf8ae74de45f2badc76
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 11:43:49 -0500

    Create InventoryLocationEnum.cs

---
+++ Scripts/Enums/Inventory/InventoryLocationEnum.cs
@@ -0,0 +1,17 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum InventoryLocationEnum {
+    Inventory = 0,
+    ToolBelt = 1,
+}}
