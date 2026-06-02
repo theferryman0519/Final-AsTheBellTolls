7709cbfe7746283c966f3cf21182821d5af392f7
The Ferryman <ferrymanstudios@gmail.com>
2026-06-02 09:25:00 -0500

    Create NpcModel.cs

---
+++ Scripts/Models/Npcs/NpcModel.cs
@@ -0,0 +1,21 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Npc {
+public class NpcModel {
+    
+#region -------------------- Variables --------------------
+    public NpcObject Npc;
+
+    public int RelationshipPoints;
+#endregion
+}}
