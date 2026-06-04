59d21136f38720c1117aefe0b6f98e1c1adcce93
The Ferryman <ferrymanstudios@gmail.com>
2026-06-04 10:28:32 -0500

    Create RelationshipPointsTypeEnum.cs

---
+++ Scripts/Enums/Npc/RelationshipPointsTypeEnum.cs
@@ -0,0 +1,29 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace Atbt.Enum {
+public enum RelationshipPointsTypeEnum {
+    AuroraWatch,
+    BirthdayGiftDisliked,
+    BirthdayGiftHated,
+    BirthdayGiftLiked,
+    BirthdayGiftLoved,
+    BirthdayGiftNeutral,
+    FirstTalk,
+    GiftDisliked,
+    GiftHated,
+    GiftLiked,
+    GiftLoved,
+    GiftNeutral,
+    SharedMeal,
+    QuestComplete,
+}}
