624c34198f5b79b04220a73c239d3b6c415f3c00
The Ferryman <ferrymanstudios@gmail.com>
2026-06-04 10:40:22 -0500

    Update ConstantController.cs

--- Scripts/Controllers/ConstantController.cs
+++ Scripts/Controllers/ConstantController.cs
@@ -10,6 +10,7 @@ using UnityEngine.SceneManagement;
 
 // Game Dependencies
 using Atbt.Core;
+using Atbt.Enum;
 
 namespace Atbt.Controller {
 public class ConstantController : Singleton<ConstantController> {
@@ -35,6 +36,11 @@ public class ConstantController : Singleton<ConstantController> {
 
     // Thresholds
     public const float Threshold_MoveInputDeadzone = 0.5f;
+
+    // Dictionaries
+    public Dictionary<QualityTypeEnum, float> QualityMultipliers;
+    public Dictionary<RelationshipPointsTypeEnum, int> RelationshipPointAdditions;
+    public Dictionary<int, int> HeartThresholds;
 #endregion
 #region -------------------- Private Variables --------------------
 
@@ -50,10 +56,71 @@ public class ConstantController : Singleton<ConstantController> {
     {
         CoreController.Inst.WriteLog(this.GetType().Name, $"Initializing the constant controller");
 
+        UpdateQualityMultipliersDict();
+        UpdateRelationshipPointAdditionsDict();
+        UpdateHeartThresholdsDict();
+
         CoreController.Inst.LoadingStepCompleted();
     }
 #endregion
 #region -------------------- Private Methods --------------------
+    private void UpdateQualityMultipliersDict()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the QualityMultipliers dictionary");
 
+        QualityMultipliers.Clear();
+
+        QualityMultipliers.Add(QualityTypeEnum.Base, 1f);
+        QualityMultipliers.Add(QualityTypeEnum.Copper, 1.1f);
+        QualityMultipliers.Add(QualityTypeEnum.Silver, 1.25f);
+        QualityMultipliers.Add(QualityTypeEnum.Gold, 1.5f);
+        QualityMultipliers.Add(QualityTypeEnum.Cobalt, 1.9f);
+    }
+
+    private void UpdateRelationshipPointAdditionsDict()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the RelationshipPointAdditions dictionary");
+
+        RelationshipPointAdditions.Clear();
+
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.AuroraWatch, 100);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.BirthdayGiftDisliked, -80);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.BirthdayGiftHated, -160);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.BirthdayGiftLiked, 80);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.BirthdayGiftLoved, 160);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.BirthdayGiftNeutral, 10);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.FirstTalk, 5);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.GiftDisliked, -20);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.GiftHated, -40);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.GiftLiked, 20);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.GiftLoved, 40);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.GiftNeutral, 0);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.SharedMeal, 30);
+        RelationshipPointAdditions.Add(RelationshipPointsTypeEnum.QuestComplete, 75);
+    }
+
+    private void UpdateHeartThresholdsDict()
+    {
+        CoreController.Inst.WriteLog(this.GetType().Name, $"Updating the HeartThresholds dictionary");
+
+        HeartThresholds.Clear();
+
+        HeartThresholds.Add(0, 0);
+        HeartThresholds.Add(1, 100);
+        HeartThresholds.Add(2, 225);
+        HeartThresholds.Add(3, 375);
+        HeartThresholds.Add(4, 550);
+        HeartThresholds.Add(5, 750);
+        HeartThresholds.Add(6, 975);
+        HeartThresholds.Add(7, 1225);
+        HeartThresholds.Add(8, 1500);
+        HeartThresholds.Add(9, 1800);
+        HeartThresholds.Add(10, 2100);
+        HeartThresholds.Add(11, 2500);
+        HeartThresholds.Add(12, 3000);
+        HeartThresholds.Add(13, 3600);
+        HeartThresholds.Add(14, 4300);
+        HeartThresholds.Add(15, 5100);
+    }
 #endregion
 }}
