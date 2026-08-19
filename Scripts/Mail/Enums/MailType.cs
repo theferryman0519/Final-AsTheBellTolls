4a62b7013029d49df1f8be5a520a9099f48b0405
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:33:40 -0500

    Add MailType enum for mail categorization

---
+++ Scripts/Mail/Enums/MailType.cs
@@ -0,0 +1,21 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Mail {
+public enum EnumsTemplate {
+    None = 0,
+    HumorNote,
+    EventReminder,
+    QuestRequest,
+    EventRequest,
+    OverflowItem,
+}}
