14b8ff9100ebc16bd851b0bc17319b6bef7a447a
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:25:06 -0500

    Add FishHabitatType enum for fishing habitats

---
+++ Scripts/Fishing/Enums/FishHabitatType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Fishing {
+public enum FishHabitatType {
+    None = 0,
+    River,
+    Lake,
+    Universal,
+}}
