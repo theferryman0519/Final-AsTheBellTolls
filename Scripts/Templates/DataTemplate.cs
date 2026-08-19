c6758045708bce358ecb24e60a20644fce2ac2e9
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 14:28:22 -0500

    Add files via upload

---
+++ Scripts/Templates/DataTemplate.cs
@@ -0,0 +1,31 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Core;
+
+namespace AsTheBellTolls.Template {
+public class DataTemplate {
+    
+#region -------------------- Variables --------------------
+#region ---------- Constant Variables ----------
+    
+#endregion
+#region ---------- Serialized Variables ----------
+    
+#endregion
+#region ---------- Public Variables ----------
+    
+#endregion
+#region ---------- Private Variables ----------
+    
+#endregion
+#endregion
+}}
---
+++ Scripts/Templates/ModelsTemplate.cs
@@ -0,0 +1,25 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Core;
+
+namespace AsTheBellTolls.Template {
+public class ModelsTemplate {
+    
+#region -------------------- Variables --------------------
+#region ---------- Public Variables ----------
+    
+#endregion
+#region ---------- Private Variables ----------
+    
+#endregion
+#endregion
+}}
