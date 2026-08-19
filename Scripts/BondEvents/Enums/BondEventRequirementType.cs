937c127dd21a4063f249f37997e32ba7fbb972d0
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:10:06 -0500

    Create BondEventRequirementType.cs

---
+++ Scripts/BondEvents/Enums/BondEventRequirementType.cs
@@ -0,0 +1,35 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.BondEvents {
+public enum BondEventRequirementType {
+    None = 0,
+    MainStoryProgress,
+    FriendshipLevel,
+    ConnectionLevel,
+    QuestState,
+    PreviousEvent,
+    ManorRestoration,
+    TownRestoration,
+    Season,
+    Day,
+    DayOfWeek,
+    Time,
+    Daylight,
+    Weather,
+    Location,
+    NpcAvailability,
+    RelationshipStatus,
+    MarriageStatus,
+    ItemPossession,
+    GameFlag,
+}}
