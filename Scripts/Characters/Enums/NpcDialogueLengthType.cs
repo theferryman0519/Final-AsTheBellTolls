ac01f7c32904308c3f0fdd20281a64886a30bf78
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:35:48 -0500

    Add NpcDialogueLengthType enum for dialogue lengths

---
+++ Scripts/Characters/Enums/NpcDialogueLengthType.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcDialogueLengthType {
+    None = 0,
+    Short,
+    Mid,
+    Long,
+    Excruciating,
+}}
