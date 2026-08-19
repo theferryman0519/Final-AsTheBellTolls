f21d24c2282b8e6654e8cf3b6e3d46c35b3365e3
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:17:36 -0500

    Add FarmingSoilStateType enum for soil states

---
+++ Scripts/Farming/Enums/FarmingSoilStateType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Farming {
+public enum FarmingSoilStateType {
+    Unavailable = 0,
+    Refueling,
+    Ready,
+    Tilled,
+}}
