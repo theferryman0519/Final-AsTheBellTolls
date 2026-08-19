238a5287f840dfd2dcd09e8ec58e3bf7a8fd8394
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:20:30 -0500

    Add AnimalType enum for various animal types

---
+++ Scripts/Animals/Enums/AnimalType.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Animals {
+public enum AnimalType {
+    None = 0,
+    Bee,
+    Cat,
+    Chicken,
+    Cow,
+    Dog,
+    Duck,
+    Goat,
+    Goose,
+    Horse,
+    Pig,
+    Sheep,
+    Silkworm,
+}}
