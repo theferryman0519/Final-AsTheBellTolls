f1edca24141a0cae65fe80fc1420d0f15bff32dc
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 14:58:36 -0500

    Create ItemType.cs

---
+++ Scripts/Items/Enums/ItemType.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Items {
+public enum ItemType {
+    None = 0,
+    Artisanal,
+    Ingredient,
+    Interactable,
+    Quest,
+    Recipe,
+    Useful,
+}}
