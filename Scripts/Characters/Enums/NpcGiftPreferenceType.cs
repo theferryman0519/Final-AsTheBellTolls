ad848e8ba473a71e604719d4b9be60b5d2de8377
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:34:17 -0500

    Create NpcGiftPreferenceType.cs

---
+++ Scripts/Characters/Enums/NpcGiftPreferenceType.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcGiftPreferenceType {
+    None = 0,
+    Favorite,
+    Loved,
+    Liked,
+    Tolerated,
+    Disliked,
+    Hated,
+}}
