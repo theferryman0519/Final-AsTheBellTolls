2751bb536396d90e92f349fae26b75bfb93a2799
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:53:20 -0500

    Add CharacterHairColorType enum with color options

--- Scripts/Characters/Enums/CharacterHairColorType.cs
+++ Scripts/Characters/Enums/CharacterHairColorType.cs
@@ -1,1 +1,34 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum CharacterHairColorType {
+    Brown = 0,
+    Black,
+    Blonde,
+    Blue,
+    DarkBrown,
+    DarkBlue,
+    DarkGreen,
+    DarkPurple,
+    DarkRed,
+    Gray,
+    Green,
+    LightBrown,
+    LightBlue,
+    LightGreen,
+    LightPurple,
+    LightRed,
+    Purple,
+    Red,
+    White,
+}}
