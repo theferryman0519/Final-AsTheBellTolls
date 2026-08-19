268b186b5830f7146651efcf5fda7f4b6b1f68ac
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:39:38 -0500

    Add NpcActivityType enum for character activities

---
+++ Scripts/Characters/Enums/NpcActivityType.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcActivityType {
+    None = 0,
+    Sleep,
+    Idle,
+    Travel,
+    Work,
+    Shopkeeping,
+    Eat,
+    Socialize,
+    Fish,
+    Read,
+    Study,
+    Explore,
+    Festival,
+    Quest,
+    Special,
+}}
