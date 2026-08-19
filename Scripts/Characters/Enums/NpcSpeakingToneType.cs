471894661a03c47147216909d0c65badaf2c52a1
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:36:43 -0500

    Add NpcSpeakingToneType enum for NPC dialogue tones

---
+++ Scripts/Characters/Enums/NpcSpeakingToneType.cs
@@ -0,0 +1,24 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcSpeakingToneType {
+    None = 0,
+    Gentle,
+    Formal,
+    Dry,
+    Reflective,
+    Compassionate,
+    Guarded,
+    Energetic,
+    Playful,
+}}
