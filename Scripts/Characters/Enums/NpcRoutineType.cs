c356862aab3aa032b8f4c9c76944429f7b8c1ba4
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:37:41 -0500

    Add NpcRoutineType enum for character routines

---
+++ Scripts/Characters/Enums/NpcRoutineType.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcRoutineType {
+    Standard = 0,
+    Seasonal,
+    Weather,
+    Special,
+    Quest,
+    Festival,
+    GameEvent,
+}}
