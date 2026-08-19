28623a518b13fcef0ee5411222d2423437cc48e6
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 14:52:39 -0500

    Add WeatherType enum for weather conditions

---
+++ Scripts/Weather/Enums/WeatherType.cs
@@ -0,0 +1,23 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Weather {
+public enum WeatherType {
+    None = 0,
+    Clear,
+    Cloudy,
+    Rainy,
+    RainySevere,
+    Snowy,
+    SnowySevere,
+    Windy,
+}}
