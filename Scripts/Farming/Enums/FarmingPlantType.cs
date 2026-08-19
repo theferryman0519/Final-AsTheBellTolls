4a292d36e167535c64da1ea3f673ed263022be81
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:18:40 -0500

    Add FarmingPlantType enum for plant categorization

---
+++ Scripts/Farming/Enums/FarmingPlantType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Farming {
+public enum FarmingPlantType {
+    None = 0,
+    GardenCrop,
+    OrchardCrop,
+    Flower,
+}}
