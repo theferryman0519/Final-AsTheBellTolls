c77d8bac07d0a4dbacaad1df28f9d22399f7a9ce
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:12:31 -0500

    Create FestivalActivityType.cs

---
+++ Scripts/Festivals/Enums/FestivalActivityType.cs
@@ -0,0 +1,27 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Festivals {
+public enum FestivalActivityType {
+    None = 0,
+    SharedMeal,
+    Fishing,
+    Market,
+    Cooking,
+    Stargazing,
+    Wedding,
+    LanternWalk,
+    Memorial,
+    Dance,
+    JudgeShow,
+    WaterSplash,
+}}
