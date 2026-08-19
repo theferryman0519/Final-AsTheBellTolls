53c5d57a5e7b15be731c16b6fd450f7f88af52fd
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:21:59 -0500

    Add AnimalLifeStageType enum for animal stages

---
+++ Scripts/Animals/Enums/AnimalLifeStageType.cs
@@ -0,0 +1,17 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Animals {
+public enum AnimalLifeStageType {
+    Baby = 0,
+    Mature,
+}}
