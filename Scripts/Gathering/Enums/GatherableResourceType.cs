2074edd43e7299bf1f34ade1ab72ea7656d758ce
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:13:15 -0500

    Create GatherableResourceType.cs

---
+++ Scripts/Gathering/Enums/GatherableResourceType.cs
@@ -0,0 +1,26 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Gathering {
+public enum GatherableResourceType {
+    None = 0,
+    FlowerDeposit,
+    GemstoneDeposit,
+    Grass,
+    HerbBush,
+    LakeWater,
+    OreDeposit,
+    RiverWater,
+    RockDeposit,
+    Tree,
+    TreeTrunk,
+}}
