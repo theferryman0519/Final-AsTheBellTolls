43a5d55a2790573e05ec975a8dfd5664b2ef51ba
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:11:20 -0500

    Add FestivalEventType enum for festival events

---
+++ Scripts/Festivals/Enums/FestivalEventType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Festivals {
+public enum FestivalEventType {
+    None = 0,
+    MainFestival,
+    MiniFestival,
+    OngoingEvent,
+}}
