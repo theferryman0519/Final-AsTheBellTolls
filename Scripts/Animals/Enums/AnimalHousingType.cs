5607dd7058bc51fb3db0593cb873ab4cfc05bb12
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:21:18 -0500

    Add AnimalHousingType enum for housing types

---
+++ Scripts/Animals/Enums/AnimalHousingType.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Animals {
+public enum AnimalHousingType {
+    None = 0,
+    Apiary,
+    Barn,
+    Coop,
+    Manor,
+    Stable,
+    WeaverCradle,
+}}
