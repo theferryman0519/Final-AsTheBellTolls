b2f8a7f2fcc4d15ff8efa82d0bf6da1a9527f338
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:00:22 -0500

    Add ArtisanalClothingType enum for clothing types

---
+++ Scripts/Items/Enums/ArtisanalClothingType.cs
@@ -0,0 +1,23 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Items {
+public enum ArtisanalClothingType {
+    None = 0,
+    Outfit,
+    Top,
+    Bottom,
+    Footwear,
+    Headwear,
+    Accessory,
+    Hairstyle,
+}}
