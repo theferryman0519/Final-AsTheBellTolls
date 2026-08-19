7d7e45409b043d78b218ccb8231aae3af69eebc2
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:08:27 -0500

    Add ItemStorageType enum for item storage options

---
+++ Scripts/Items/Enums/ItemStorageType.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Items {
+public enum ItemStorageType {
+    None = 0,
+    ToolBelt,
+    Satchel,
+    Pantry,
+    Mailbox,
+    ByproductBox,
+    StorageUnit,
+}}
