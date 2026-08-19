5166a41f68ff81f36e0553043d196c77c72c8426
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:38:27 -0500

    Create NpcIdleType.cs

---
+++ Scripts/Characters/Enums/NpcIdleType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcIdleType {
+    None = 0,
+    Stand,
+    Wander,
+    Service,
+}}
