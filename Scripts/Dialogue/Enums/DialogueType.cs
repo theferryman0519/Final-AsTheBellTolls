1d92667d1a825f41b822f01da878986ab3ecf23c
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:37:13 -0500

    Add DialogueType enum for dialogue categorization

---
+++ Scripts/Dialogue/Enums/DialogueType.cs
@@ -0,0 +1,30 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Dialogue {
+public enum DialogueType {
+    None = 0,
+    Generic,
+    Festival,
+    Gift,
+    Quest,
+    BondEvent,
+    ConnectionEvent,
+    EdwardHelp,
+    Shopping,
+    FirstMeet,
+    Proposal,
+    Greeting,
+    MarriedLife,
+    Intro,
+    Repossession,
+}}
