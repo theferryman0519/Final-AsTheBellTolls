217fb2af838a48d965bf364538235139ae277eaf
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:05:44 -0500

    Add QuestObjectiveType enum for quest objectives

---
+++ Scripts/Quests/Enums/QuestObjectiveType.cs
@@ -0,0 +1,29 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Quests {
+public enum QuestObjectiveType {
+    None = 0,
+    Talk,
+    Collect,
+    Deliver,
+    Craft,
+    Cook,
+    Fish,
+    Restore,
+    Purchase,
+    ReachLocation,
+    ReachFriendship,
+    ReachConnection,
+    CompleteEvent,
+    Custom,
+}}
