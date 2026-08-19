0e83450788eaf55daf8d9112fcd53011183548de
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:57:32 -0500

    Add CharacterAgeRangeType enum for age categories

---
+++ Scripts/Characters/Enums/CharacterAgeRangeType.cs
@@ -0,0 +1,23 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum CharacterAgeRangeType {
+    Twenties = 0,
+    Child,
+    Teens,
+    Thirties,
+    Forties,
+    Fifties,
+    Sixties,
+    Seventies,
+}}
