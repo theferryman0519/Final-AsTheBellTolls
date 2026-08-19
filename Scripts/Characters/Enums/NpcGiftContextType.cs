e6659531561b3941ee109de6ef5d7d6ecce93b5b
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:34:52 -0500

    Add NpcGiftContextType enum for gift contexts

---
+++ Scripts/Characters/Enums/NpcGiftContextType.cs
@@ -0,0 +1,17 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Characters {
+public enum NpcGiftContextType {
+    Normal = 0,
+    Birthday,
+}}
