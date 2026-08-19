3c88e3de32af6032160f51f95fd2427a4e64f952
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:15:23 -0500

    Create InteractionType.cs

---
+++ Scripts/Interaction/Enums/InteractionType.cs
@@ -0,0 +1,40 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Interaction {
+public enum InteractionType {
+    None = 0,
+    Talk,
+    Shop,
+    Gift,
+    Harvest,
+    Gather,
+    Fish,
+    Plant,
+    Water,
+    Till,
+    Chop,
+    Mine,
+    Cut,
+    Enter,
+    Exit,
+    Read,
+    Open,
+    Use,
+    Cook,
+    Craft,
+    MakeTonic,
+    Restore,
+    Customize,
+    Store,
+    Travel,
+}}
