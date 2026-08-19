3c0b68305314ada2890cb932fb9586eba039d38b
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 16:30:08 -0500

    Create MinigameResultType.cs

---
+++ Scripts/Activities/Enums/MinigameResultType.cs
@@ -0,0 +1,19 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Activities {
+public enum MinigameResultType {
+    None = 0,
+    Success,
+    Failure,
+    Cancelled,
+}}
