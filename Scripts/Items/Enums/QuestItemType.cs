9496769ad415312ba6e9edf86cd912c70b8400b8
The Ferryman <ferrymanstudios@gmail.com>
2026-08-19 15:03:25 -0500

    Add QuestItemType enum for item categorization

---
+++ Scripts/Items/Enums/QuestItemType.cs
@@ -0,0 +1,17 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Items {
+public enum QuestItemType {
+    None = 0,
+    Recovery,
+}}
