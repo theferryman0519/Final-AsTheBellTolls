514904f143eb01f3f09e4b1e2c24199efe259c9b
The Ferryman <ferrymanstudios@gmail.com>
2026-08-20 15:17:50 -0500

    Add MinigameTonicMakingColors enum for color options

---
+++ Scripts/Activities/Enums/MinigameTonicMakingColors.cs
@@ -0,0 +1,20 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Activities {
+public enum MinigameTonicMakingColors {
+    Red = 0,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+}}
