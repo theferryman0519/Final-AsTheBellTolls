323a7b45accf2f78778ba611eaeab104e8efd106
The Ferryman <ferrymanstudios@gmail.com>
2026-08-20 09:21:28 -0500

    Add AnimalController class for animal management

---
+++ Scripts/Animals/Controller/AnimalController.cs
@@ -0,0 +1,63 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+using AsTheBellTolls.Core;
+
+namespace AsTheBellTolls.Animals {
+public class AnimalController : Singleton<AnimalController> {
+    
+#region -------------------- Variables --------------------
+#region ---------- Constant Variables ----------
+    
+#endregion
+#region ---------- Serialized Variables ----------
+    
+#endregion
+#region ---------- Public Variables ----------
+    
+#endregion
+#region ---------- Private Variables ----------
+    
+#endregion
+#endregion
+#region -------------------- Coroutines --------------------
+#region ---------- Public Coroutines ----------
+    
+#endregion
+#region ---------- Private Coroutines ----------
+    
+#endregion
+#endregion
+#region -------------------- Methods --------------------
+#region ---------- Unity Methods ----------
+    void Awake()
+    {
+        //
+    }
+
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        //
+    }
+#endregion
+#region ---------- Public Methods ----------
+    
+#endregion
+#region ---------- Private Methods ----------
+    
+#endregion
+#endregion
+}}
