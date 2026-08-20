2486b95289bab0c8050f87c3ed338ae34fbbf8fd
The Ferryman <ferrymanstudios@gmail.com>
2026-08-20 15:11:50 -0500

    Create MinigameProgress.cs

---
+++ Scripts/Activities/Models/MinigameProgress.cs
@@ -0,0 +1,22 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Activities {
+public class MinigameProgress {
+    
+#region -------------------- Variables --------------------
+#region ---------- Public Variables ----------
+    public MinigameType ActivityType { get; set; }
+    public MinigameResultType ActivityResult { get; set; }
+#endregion
+#endregion
+}}
