6217c1beec53e8c906b60cb729300590800fcc07
The Ferryman <ferrymanstudios@gmail.com>
2026-08-20 14:53:02 -0500

    Add MinigameTonicMakingConstants class with constants

---
+++ Scripts/Activities/Data/MinigameTonicMakingConstants.cs
@@ -0,0 +1,28 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+
+namespace AsTheBellTolls.Activities {
+public class MinigameTonicMakingConstants {
+    
+#region -------------------- Variables --------------------
+#region ---------- Constant Variables ----------
+    // Herb Amounts
+    public const int MinHerbAmount = 1;
+    public const int MaxHerbAmount = 5;
+
+    // Timers
+    public const float MinColorToColor = 0.5f;
+    public const float MaxColorToColor = 2.5f;
+    public const float MaxValveTime = 3.0f;
+#endregion
+#endregion
+}}
